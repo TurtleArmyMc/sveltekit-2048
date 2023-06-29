5070a3761aed35abfebe229faf4b328e9ebfae8f
TurtleArmyMc <44322335+TurtleArmyMc@users.noreply.github.com>
2023-06-29 11:08:25 -0400

    Initial commit

---
+++ src/routes/2048/+page.svelte
@@ -0,0 +1,120 @@
+<script lang="ts">
+    const COLOR_CLASSES = [
+        "cell_empty",
+        "cell2",
+        "cell4",
+        "cell8",
+        "cell16",
+        "cell32",
+        "cell64",
+        "cell128",
+        "cell256",
+        "cell512",
+        "cell1024",
+        "cell2048",
+    ];
+
+    import { Game } from "./game";
+    let game = new Game();
+    let board = game.board;
+
+    function keydown(event: KeyboardEvent) {
+        switch (event.key) {
+            case "ArrowDown":
+                game.makeMove("down");
+                board = game.board;
+                break;
+            case "ArrowUp":
+                game.makeMove("up");
+                board = game.board;
+                break;
+            case "ArrowLeft":
+                game.makeMove("left");
+                board = game.board;
+                break;
+            case "ArrowRight":
+                game.makeMove("right");
+                board = game.board;
+                break;
+            default:
+                break;
+        }
+    }
+</script>
+
+<svelte:window on:keydown={keydown} />
+
+<p>Hello world!</p>
+<br />
+
+<div class="board">
+    {#each board as row}
+        <div class="row">
+            {#each row as e}
+                <div class="cell">
+                    <div
+                        class={"visible_cell" +
+                            (e < COLOR_CLASSES.length
+                                ? COLOR_CLASSES[e]
+                                : "cell_big")}
+                    >
+                        {e ? 2 ** e : ""}
+                    </div>
+                </div>
+            {/each}
+        </div>
+    {/each}
+</div>
+
+<style>
+    .row {
+        display: flex;
+    }
+
+    .cell {
+        height: 40px;
+        width: 40px;
+        padding: 0px;
+        margin: 5px;
+    }
+
+    .cell_empty {
+        background: beige;
+    }
+    .cell2 {
+        background: hsl(35 10% 80%);
+    }
+    .cell4 {
+        background: hsl(35 20% 80%);
+    }
+    .cell8 {
+        background: hsl(35 30% 80%);
+    }
+    .cell16 {
+        background: hsl(35 40% 80%);
+    }
+    .cell32 {
+        background: hsl(35 50% 80%);
+    }
+    .cell64 {
+        background: hsl(35 60% 80%);
+    }
+    .cell128 {
+        background: hsl(35 70% 80%);
+    }
+    .cell256 {
+        background: hsl(35 80% 80%);
+    }
+    .cell512 {
+        background: hsl(35 90% 80%);
+    }
+    .cell1024 {
+        background: hsl(35 100% 80%);
+    }
+    .cell2048 {
+        background: hsl(35 100% 80%);
+    }
+    .cell_big {
+        background: hsl(35 100% 80%);
+    }
+</style>
---
+++ src/routes/2048/game.ts
@@ -0,0 +1,183 @@
+const n: number = 4;
+
+export type Move = "up" | "down" | "left" | "right";
+
+
+function offsetFor(move: Move): [number, number] {
+    const rOffset = move == "up" ? -1 : move == "down" ? 1 : 0;
+    const cOffset = move == "left" ? -1 : move == "right" ? 1 : 0;
+    return [rOffset, cOffset];
+}
+
+export class Game {
+    board: number[][];
+    prevBoard: number[][];
+    /** How far the tile that was in this position moved away (in tiles) */
+    // moveAwayDistance: number[][];
+    // /** How far the tile that was in this position moved away (in tiles) */
+    // moveToDistance: number[][];
+
+
+    constructor() {
+        this.board = [];
+        this.prevBoard = [];
+        // this.moveAwayDistance = [];
+        for (let i = 0; i < n; i++) {
+            this.board.push(Array(n).fill(0));
+            this.prevBoard.push(Array(n).fill(0));
+            // this.moveAwayDistance.push(Array(n).fill(0));
+        }
+        this.spawnNewTile();
+    }
+
+    moveIsValid(move: Move) {
+        const [rOffset, cOffset] = offsetFor(move);
+
+        for (let r = 0; r < n; r++) {
+            for (let c = 0; c < n; c++) {
+                const v = this.board[r][c];
+                if (v == 0) {
+                    continue;
+                }
+                const r2 = r + rOffset;
+                const c2 = c + cOffset;
+                if (0 <= r2 && 0 <= c2 && r2 < n && c2 < n) {
+                    // A tile that is empty or can be combined in the direction
+                    // of the move with the current tile
+                    if (this.board[r2][c2] == v || this.board[r2][c2] == 0) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    gameOver(): boolean {
+        return !this.moveIsValid("up") && !this.moveIsValid("down") && !this.moveIsValid("left") && !this.moveIsValid("right");
+    }
+
+    makeMove(move: Move): boolean {
+        if (!this.moveIsValid(move)) {
+            return false;
+        }
+
+        this.setPrevBoard();
+
+        if (move == "up") {
+            for (let ri = 1; ri < n; ri++) {
+                let mergedLast = false;
+                for (let c = 0; c < n; c++) {
+                    const v = this.board[ri][c];
+                    this.board[ri][c] = 0;
+                    let rf = ri;
+                    while (rf > 0 && this.board[rf][c] == 0 && this.board[rf - 1][c] == 0) {
+                        rf--;
+                    }
+
+                    if (rf > 0 && this.board[rf - 1][c] == v && !mergedLast) {
+                        this.board[rf - 1][c] = v + 1;
+                        mergedLast = true;
+                    } else {
+                        this.board[rf][c] = v;
+                        mergedLast = false;
+                    }
+                }
+            }
+        } else if (move == "down") {
+            for (let ri = n - 2; ri >= 0; ri--) {
+                let mergedLast = false;
+                for (let c = 0; c < n; c++) {
+                    const v = this.board[ri][c];
+                    this.board[ri][c] = 0;
+                    let rf = ri;
+                    while (rf < n - 1 && this.board[rf][c] == 0 && this.board[rf + 1][c] == 0) {
+                        rf++;
+                    }
+
+                    if (rf < n - 1 && this.board[rf + 1][c] == v && !mergedLast) {
+                        this.board[rf + 1][c] = v + 1;
+                        mergedLast = true;
+                    } else {
+                        this.board[rf][c] = v;
+                        mergedLast = false;
+                    }
+                }
+            }
+        } else if (move == "left") {
+            for (let r = 0; r < n; r++) {
+                let mergedLast = false;
+                for (let ci = 1; ci < n; ci++) {
+                    const v = this.board[r][ci];
+                    this.board[r][ci] = 0;
+                    let cf = ci;
+                    while (cf > 0 && this.board[r][cf] == 0 && this.board[r][cf - 1] == 0) {
+                        cf--;
+                    }
+
+                    if (cf > 0 && this.board[r][cf - 1] == v && !mergedLast) {
+                        this.board[r][cf - 1] = v + 1;
+                        mergedLast = true;
+                    } else {
+                        this.board[r][cf] = v;
+                        mergedLast = false;
+                    }
+                }
+            }
+        } else if (move == "right") {
+            for (let r = 0; r < n; r++) {
+                let mergedLast = false;
+                for (let ci = n - 2; ci >= 0; ci--) {
+                    const v = this.board[r][ci];
+                    this.board[r][ci] = 0;
+                    let cf = ci;
+                    while (cf < n - 1 && this.board[r][cf] == 0 && this.board[r][cf + 1] == 0) {
+                        cf++;
+                    }
+
+                    if (cf < n - 1 && this.board[r][cf + 1] == v && !mergedLast) {
+                        this.board[r][cf + 1] = v + 1;
+                        mergedLast = true;
+                    } else {
+                        this.board[r][cf] = v;
+                        mergedLast = false;
+                    }
+                }
+            }
+        }
+
+        this.spawnNewTile();
+
+        return true;
+    }
+
+    // Returns whether a number was successfully spawned
+    private spawnNewTile(): boolean {
+        const blankCoords: [number, number][] = [];
+
+        for (let r = 0; r < n; r++) {
+            for (let c = 0; c < n; c++) {
+                if (this.board[r][c] == 0) {
+                    blankCoords.push([r, c]);
+                }
+            }
+        }
+
+        if (blankCoords.length) {
+            this.setPrevBoard();
+
+            const [r, c] = blankCoords[Math.floor(Math.random() * blankCoords.length)];
+            // TODO: Should also sometimes be a 4
+            this.board[r][c] = 1;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private setPrevBoard() {
+        this.prevBoard = [];
+        this.board.forEach(row => this.prevBoard.push([...row]));
+    }
+}
