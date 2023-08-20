414817c62f4f0708f9f8df1924132acdac4e111d
TurtleArmyMc <44322335+TurtleArmyMc@users.noreply.github.com>
2023-08-20 16:35:52 -0400

    feat: Add touch support feat: Add header bar fix: Incorrect z-heights fix: Pulldown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
     import { cubicIn } from "svelte/easing";
     import { fade, scale } from "svelte/transition";
     import { browser } from "$app/environment";
-    import { Game, moveOffset } from "./game";
+    import { Game, moveOffset, type Move } from "./game";
 
     let game = new Game();
     const animProgress = tweened(1, {
@@ -22,22 +22,8 @@
         game = game;
     });
 
-    function keydown(event: KeyboardEvent) {
-        let newGame = undefined;
-        switch (event.key) {
-            case "ArrowDown":
-                newGame = game.makeMove("down");
-                break;
-            case "ArrowUp":
-                newGame = game.makeMove("up");
-                break;
-            case "ArrowLeft":
-                newGame = game.makeMove("left");
-                break;
-            case "ArrowRight":
-                newGame = game.makeMove("right");
-                break;
-        }
+    function tryMove(move: Move) {
+        let newGame = game.makeMove(move);
         if (newGame !== undefined) {
             game = newGame;
             animProgress.set(0, { duration: 0 });
@@ -45,70 +31,131 @@
         }
     }
 
-    const TRANSLATE_MULT = (1 + 2 / 12) * 100;
+    function keydown(event: KeyboardEvent) {
+        switch (event.key) {
+            case "ArrowDown":
+                tryMove("down");
+                break;
+            case "ArrowUp":
+                tryMove("up");
+                break;
+            case "ArrowLeft":
+                tryMove("left");
+                break;
+            case "ArrowRight":
+                tryMove("right");
+                break;
+        }
+    }
+
+    let swipeStartX: number;
+    let swipeStartY: number;
+    function touchstart(event: TouchEvent) {
+        console.log(event);
+
+        swipeStartX = event.changedTouches[0].screenX;
+        swipeStartY = event.changedTouches[0].screenY;
+    }
+
+    function touchend(event: TouchEvent) {
+        console.log(event);
+
+        const xDist = event.changedTouches[0].screenX - swipeStartX;
+        const yDist = event.changedTouches[0].screenY - swipeStartY;
+
+        console.log(xDist);
+        console.log(yDist);
+
+        if (xDist == 0 && yDist == 0) return;
+
+        if (Math.abs(xDist) > Math.abs(yDist)) {
+            tryMove(xDist < 0 ? "left" : "right");
+        } else {
+            tryMove(yDist < 0 ? "up" : "down");
+        }
+    }
+
+    // A constant needed to convert translation distances in terms of tiles
+    // moved to a translation percent that takes tile margins into account
+    const TRANSLATE_MULT = 110;
 </script>
 
 <svelte:window on:keydown={keydown} />
 
-<div class="game_wrapper">
-    <div class="board">
-        {#each game.board as row, r}
-            {@const [rOffsetMult, cOffsetMult] = moveOffset(game.prevMove)}
-            {@const tilesMoved = $animProgress * game.n}
-            {@const rTilesMoved = tilesMoved * rOffsetMult}
-            {@const cTilesMoved = tilesMoved * cOffsetMult}
-            <div class="row">
-                {#each row as tileVal, c}
-                    {@const arriveDist = game.arriveDist[r][c]}
-                    {@const sendDist = game.sendDist[r][c]}
-                    {@const prevVal = game.prevBoard[r][c]}
+<svelte:document
+    on:touchstart|preventDefault={touchstart}
+    on:touchend|preventDefault={touchend}
+/>
 
-                    <div class="board_cell">
-                        {#if tileVal != 0 && tilesMoved >= arriveDist}
-                            <!-- A tile that is in this position and has finished moving and being merged with -->
-                            <div
-                                class="tile tile{tileVal}"
-                                in:scale={{
-                                    duration: 100,
-                                    easing: (n) => -0.25 * n * (n - 1) + 1,
-                                }}
-                            >
-                                <!-- Current local tile -->
-                                {tileVal}
-                            </div>
-                        {:else if prevVal != 0 && sendDist == 0}
-                            <!-- A tile that was here and is waiting for the merge animation of the arriving tile -->
-                            <div class="tile tile{prevVal}">
-                                <!-- Previous tile waiting to get merged -->
-                                {prevVal}
-                            </div>
-                        {/if}
+<div class="header">
+    <b>2048</b>
+</div>
 
-                        {#if prevVal != 0 && tilesMoved < sendDist}
-                            <!-- A tile that was moved away and is being animated moving -->
-                            <div
-                                class="tile tile{prevVal}"
-                                style="transform: translate({TRANSLATE_MULT *
-                                    cTilesMoved}%, {TRANSLATE_MULT *
-                                    rTilesMoved}%);"
-                            >
-                                <!-- Old, moving tile -->
-                                {prevVal}
-                            </div>
-                        {/if}
-                    </div>
-                {/each}
-            </div>
-        {/each}
-    </div>
+<div class="board">
+    {#each game.board as row, r}
+        {@const [rOffsetMult, cOffsetMult] = moveOffset(game.prevMove)}
+        {@const tilesMoved = $animProgress * game.n}
+        {@const rTilesMoved = tilesMoved * rOffsetMult}
+        {@const cTilesMoved = tilesMoved * cOffsetMult}
+        <div class="row">
+            {#each row as tileVal, c}
+                {@const arriveDist = game.arriveDist[r][c]}
+                {@const sendDist = game.sendDist[r][c]}
+                {@const prevVal = game.prevBoard[r][c]}
+
+                <div class="board_cell">
+                    {#if tileVal != 0 && tilesMoved >= arriveDist}
+                        <!-- A tile that is in this position and has finished moving and being merged with -->
+                        <div
+                            class="tile tile{tileVal}"
+                            in:scale={{
+                                duration: 100,
+                                easing: (n) => -0.25 * n * (n - 1) + 1,
+                            }}
+                        >
+                            <!-- Current local tile -->
+                            {tileVal}
+                        </div>
+                    {:else if prevVal != 0 && sendDist == 0}
+                        <!-- A tile that was here and is waiting for the merge animation of the arriving tile -->
+                        <div class="tile tile{prevVal}">
+                            <!-- Previous tile waiting to get merged -->
+                            {prevVal}
+                        </div>
+                    {/if}
+
+                    {#if prevVal != 0 && tilesMoved < sendDist}
+                        <!-- A tile that was moved away and is being animated moving -->
+                        <div
+                            class="tile tile{prevVal}"
+                            style="transform: translate({TRANSLATE_MULT *
+                                cTilesMoved}%, {TRANSLATE_MULT *
+                                rTilesMoved}%);"
+                        >
+                            <!-- Old, moving tile -->
+                            {prevVal}
+                        </div>
+                    {/if}
+                </div>
+            {/each}
+        </div>
+    {/each}
     {#if browser && lost}
-        <div class="loss_popup" in:fade><p>You lost!</p></div>
+        <div class="loss_popup" in:fade><p>Game<br />Over!</p></div>
     {/if}
 </div>
 
 <style>
-    .game_wrapper {
-        position: relative;
+    :root {
+        font-family: Arial, sans-serif;
+    }
+
+    .header {
+        font-size: max(11.25vmin, 22.5px);
+        width: max(80vmin, 160px);
+        margin: auto;
+        padding-bottom: 3vh;
+        /* background-color: ; */
     }
 
     .board {
@@ -121,18 +168,15 @@
     }
 
     .loss_popup {
-        background-color: #a0808080;
+        background-color: #a0a0a0c5;
         position: absolute;
         width: 100%;
         height: 100%;
         top: 0px;
         left: 0px;
-    }
-
-    .loss_popup p {
-        position: absolute;
-        top: 50%;
+        font-size: max(min(20vmin, 200px), 40px);
         text-align: center;
+        z-index: 2;
     }
 
     .row {
@@ -160,8 +204,8 @@
         text-align: center;
         font-size: max(9vmin, 18px);
         line-height: 200%;
-        font-family: Arial, sans-serif;
         font-weight: bold;
+        z-index: 1;
     }
 
     .tile2 {
